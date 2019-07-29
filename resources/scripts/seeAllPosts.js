32e0fa7e1f031b342de786bd0f49dc8933d4c0d6
Chris-McQuiggan <chris@mcquiggan.info>
2019-07-29 11:48:06 +0100

    add elements

--- resources/scripts/seeAllPosts.js
+++ resources/scripts/seeAllPosts.js
@@ -1,7 +1,27 @@
-async function getAllPosts(){
+async function getAllPosts() {
 
     await makeRequest("GET", "/getAll").then(data => {
         console.log(data);
+        showAllPosts(data);
     });
 
+}
+
+function showAllPosts(data) {
+    let post = JSON.parse(data);
+    for (let i = 0; i < post.length; i++) {
+        let newPostHead = document.createElement('div');
+        newPostHead.innerText = post[i].username;
+        newPostHead.id = post[i].id + "head";
+        document.getElementById("allPosts").appendChild(newPostHead);
+    
+        let newPostContent = document.createElement('div');
+        newPostContent.innerText = post[i].content;
+        document.getElementById("allPosts").appendChild(newPostContent);
+
+        let newPostDelete = document.createElement('button');
+        newPostDelete.id = post[i].id;
+        newPostDelete.onclick = deletePost(post[i].id);
+
+    }
 }
