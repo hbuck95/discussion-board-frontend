150d6cb7a6ddc34b62965a20e618e4cf961ca668
Harry Buck <harry-buck@hotmail.co.uk>
2019-07-29 11:46:41 +0100

    Specified JSON header

--- resources/scripts/util.js
+++ resources/scripts/util.js
@@ -4,7 +4,9 @@ async function makeRequest(method, url, body) {
     return new Promise((res, rej) => {
         const req = new XMLHttpRequest();
         req.open(method, API + url);
+        req.setRequestHeader("Content-Type", "application/json");
 
+        console.log(body);
         req.onload = () => {
             if (req.status >= 200 && req.status < 300) {
                 res(req.responseText);
