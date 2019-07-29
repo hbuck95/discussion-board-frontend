5ce16cc1d9a4f65165694b69f0333e43217407e7
jm4clark <jamesmclark96@gmail.com>
2019-07-29 12:04:31 +0100

    delete func refined more

--- delete.js
+++ delete.js
@@ -15,11 +15,15 @@ function makeRequest(requestType, url, sendData) {
 }
 
 
-function deletePost(id){
-
-    return makeRequest("DELETE", `/delete`).then((req) => {
+function deletePost(id){  
+    let jsonData = {
+        _id: id,
+        email: createEmail.value
+    }
+    
+    return makeRequest("DELETE", `/delete`, jsonData).then((req) => {
         console.log("It worked!");
-        // let toDelete = document.getElementById(id);
-        // toDelete.parentNode.removeChild(toDelete);
+        let toDelete = document.getElementById(id);
+        toDelete.parentNode.removeChild(toDelete);
     }).catch((err) => { console.log(err) });
 }
